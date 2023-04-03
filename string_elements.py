e5a558d8c97251596140ac0287204488160754a5
Mo <charlybillion@Mo.local>
2023-04-03 12:57:30 +0100

    string element

---
+++ string_elements.py
@@ -0,0 +1,14 @@
+str1 = "Python String"
+print(str1[0]) # P
+print(str1[1]) # y
+
+str2 = "Python String"
+print(str2[-1])  # g
+print(str2[-2])  # n
+
+#The following illustrates the indexes of the string "Python String":
+# +---+---+---+---+---+---+---+---+---+---+---+---+---+
+# | P | y | t | h | o | n |   | S | t | r | i | n | g | 
+# +---+---+---+---+---+---+---+---+---+---+---+---+---+
+#   0   1   2   3   4   5   6   7   8   9   10  11  12
+# -13  -12  -11  -10 -9  -8  -7  -6  -5  -4  -3  -2  -1 
