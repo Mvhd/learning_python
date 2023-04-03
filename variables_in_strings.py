1b194ff5ab06fcc5e88457379d46a293ea0f5428
Mo <charlybillion@Mo.local>
2023-04-03 12:48:58 +0100

    variables in strings

---
+++ variables_in_strings.py
@@ -0,0 +1,7 @@
+name = 'John'
+message = 'Hi'
+
+#To do it, you place the letter f before the opening quotation mark and put the brace around the variable name
+name = 'John'
+message = f'Hi {name}'
+print(message)
