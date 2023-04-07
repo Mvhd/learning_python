b98e2a8e221b5a45a74baaec419c4635d05820c8
Mo <charlybillion@Mo.local>
2023-04-07 14:53:23 +0100

    conversion

--- operators.py
+++ operators.py
@@ -10,3 +10,18 @@ print(strvalue1,strvalue2)
 
 # The expression 'apple' < 'orange' returns True because the letter a in apple is before the letter o in orange.
 # Similarly, the 'banana' < 'apple' returns False because the letter 'b' is after the letter 'a'.
+numvalue3 = 20 >= 10 #True
+numvalue4 = 20 >= 20 #True
+numvalue5 = 10 >= 20 #False
+
+20 == 10 #False
+20 == 20 #True
+
+'apple' == 'apple' #True
+'apple' == 'orange' #False
+
+20 != 20 #False
+20 != 10 #True
+
+'apple' != 'apple' #False
+'apple' != 'orange' #True
