a02a186fa7f04c59755c7f306ac282d673071984
Mo <charlybillion@Mo.local>
2023-04-07 09:23:52 +0100

    underscore numbers

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # learning_python
+**All learning process was pushed to github and example codes copied directly/lifted from [Python Tutirial Dot Net](https://www.pythontutorial.net/)**
---
+++ underscores.py
@@ -0,0 +1,6 @@
+#When a number is large, it’ll become difficult to read. For example:
+count1 = 10000000000
+# To make the long numbers more readable, you can group digits using underscores, like this:
+count2 = 10_000_000_000
+# When storing these values, Python just ignores the underscores. It does so when displaying the numbers with underscores on the screen:
+print(count2)
