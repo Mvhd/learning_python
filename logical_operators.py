5cf7d04e6ecacbda9c47fddc73ef75919dca1d4b
Mo <charlybillion@Mo.local>
2023-04-07 15:00:32 +0100

    Logical Operator

---
+++ logical_operators.py
@@ -0,0 +1,17 @@
+# Python has three logical operators:
+# and
+# or
+# not
+
+price = 9.99
+price > 9 and price < 10 #True
+price > 10 and price < 20 #False
+
+# The not operator
+a = True
+not a
+
+price = 9.99
+not price > 10 #True
+
+not (price > 5 and price < 10) #False
