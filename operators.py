40f7a98fabdb29831152da4b4fbe2ecec704b0ce
Mo <charlybillion@Mo.local>
2023-04-07 14:37:19 +0100

    operators

---
+++ operators.py
@@ -0,0 +1,12 @@
+numvalue1 = 10 < 20 #True
+numvalue2 = 30 < 20 #False
+
+print(numvalue1,numvalue2)
+
+strvalue1 = 'apple' < 'orange' #True
+strvalue2 = 'banana' < 'apple' #False
+
+print(strvalue1,strvalue2)
+
+# The expression 'apple' < 'orange' returns True because the letter a in apple is before the letter o in orange.
+# Similarly, the 'banana' < 'apple' returns False because the letter 'b' is after the letter 'a'.
