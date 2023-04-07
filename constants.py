e3c80e93f04143d4678bb918ede7913c6a4a15b3
Mo <charlybillion@Mo.local>
2023-04-07 11:20:04 +0100

    constants

---
+++ constants.py
@@ -0,0 +1,3 @@
+# The bad news is that Python doesn’t support constants.
+# To work around this, you use all capital letters to name a variable to indicate that the variable should be treated as a constant
+FILE_SIZE_LIMIT = 2000
