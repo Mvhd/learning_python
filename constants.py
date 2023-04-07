0f073da002faa9569a3d91bfc8da3166a5282cfd
Mo <charlybillion@Mo.local>
2023-04-07 11:20:58 +0100

    constants

--- constants.py
+++ constants.py
@@ -1,3 +1,7 @@
 # The bad news is that Python doesn’t support constants.
 # To work around this, you use all capital letters to name a variable to indicate that the variable should be treated as a constant
 FILE_SIZE_LIMIT = 2000
+
+# Summary
+# Python doesn’t have built-in constant types.
+# By convention, Python uses a variable whose name contains all capital letters to define a constant.
