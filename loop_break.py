dbf5216c65539a713620433b6b1137bdfdf3a555
Mo <charlybillion@Mo.local>
2023-04-10 16:59:36 +0100

    Loop Break

--- loop_break.py
+++ loop_break.py
@@ -4,3 +4,11 @@ for index in range(0, 10):
     print(index)
     if index == 7:
         break
+
+for x in range(5):
+    for y in range(5):
+        # terminate the innermost loop
+        if y > 1:
+            break
+        # show coordinates on the screen
+        print(f"({x},{y})")
